7bd54a5c19b2db236acfd65e81edf47e9b04b712
pavan1803 <rockstarpavan0071@gmail.com>
2026-04-24 18:38:17 +0530

    back to sqllite3

--- movie/settings.py
+++ movie/settings.py
@@ -83,7 +83,10 @@ WSGI_APPLICATION = 'movie.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("postgresql://moviedb_shgr_user:uK91S6XZtlJ4uxhcyyIV5Gh6qLIJBoXh@dpg-d7lluv3eo5us73dnq6ag-a/moviedb_shgr"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
